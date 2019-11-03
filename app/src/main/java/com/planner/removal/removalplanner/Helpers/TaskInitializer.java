cd76039c621fea51525d47ab9595a91948a4c182
Robert Lange <robert.lange.81@gmail.com>
2019-11-04 00:12:54 +0100

    init tasks for phone, tv, internet

--- app/src/main/java/com/planner/removal/removalplanner/Helpers/TaskInitializer.java
+++ app/src/main/java/com/planner/removal/removalplanner/Helpers/TaskInitializer.java
@@ -35,14 +35,30 @@ public class TaskInitializer {
         rentalContractNew.addLink("Türschilder aussuchen", "https://www.amazon.de/s?k=hausschilder+mit+namen&adgrpid=69976276766&gclid=Cj0KCQjwr-_tBRCMARIsAN413WTWhp80ZVdRs4LlF2dEewnOedLgufrRhydV9qZpyuDeoZXtbCbRPpYaAnsZEALw_wcB&hvadid=391551507346&hvdev=c&hvlocphy=9042976&hvnetw=g&hvpos=1t1&hvqmt=b&hvrand=1850260414947884646&hvtargid=kwd-299359190093&hydadcr=27958_1978105&tag=googhydr08-21&ref=pd_sl_35u1b84rff_b");
         Task.addTask(informOldAndNewNeighbours);
 
-        Task informAuthoritiesAndContractors = new Task("Ummeldung Behörden etc.", "Nach Ihrem Umzug (aber nicht davor!) müssen Sie sich in Ihrem Einwohnermeldeamt ummelden. Geben Sie auch Ihre neue Adresse an Ihre Vertragspartner weiter und verlasen Sie sich nicht auf den Nachsendeauftrag:" +
-                "\n* Einwohnermeldeamt" +
-                "\n* KFZ, Parkausweis, Umwelt-Plakette" +
-                "\n* Arbeitgeber" +
-                "\n* Bank(en) und Versicherungen", addMonthDaysToJavaUtilDate(removalDate, 0, 13), Priority.High, 0L, TaskType.ORGANISATION);
-        informAuthoritiesAndContractors.addLink("Wohnsitz ummelden", "https://umziehen.de/an-ab-ummelden/wohnsitz-anmelden-192");
-        informAuthoritiesAndContractors.addLink("Auto ummelden", "https://www.umzug.de/tipps/ummelden/auto-ummelden.html");
-        Task.addTask(informAuthoritiesAndContractors);
+        Task informAuthorities = new Task("Ummeldung Behörden etc.", "Nach Ihrem Umzug (aber nicht davor!) müssen Sie sich in Ihrem Einwohnermeldeamt ummelden. Geben Sie auch Ihre neue Adresse an Ihre Vertragspartner weiter und verlassen Sie sich nicht auf den Nachsendeauftra.", addMonthDaysToJavaUtilDate(removalDate, 0, 13), Priority.High, 0L, TaskType.ORGANISATION);
+        informAuthorities.addLink("Wohnsitz ummelden", "https://umziehen.de/an-ab-ummelden/wohnsitz-anmelden-192");
+        informAuthorities.addLink("Auto ummelden / neue Plakette", "https://www.umzug.de/tipps/ummelden/auto-ummelden.html");
+        Task.addTask(informAuthorities);
+
+        Task informContractors = new Task("Vertragspartner informieren", "Informieren Sie Banken, Versicherungen, Mobilfunkanbieter, Arbeitgeber und andere Vertragspartner über Ihre neue Adresse. verlassen Sie sich nicht nur auf den Nachsendeauftrag", addMonthDaysToJavaUtilDate(removalDate, 0, -14), Priority.Normal, 0L, TaskType.ORGANISATION);
+        Task.addTask(informContractors);
+
+        Task currentContract = new Task("Energieversorger", "Prüfen Sie, ob Sie Ihren alten Gas - bzw. Stromvertrag behalten (aber über Adresswechsel informieren) oder wechseln müssen / dürfen.", addMonthDaysToJavaUtilDate(removalDate, 0, -7), Priority.High, 0L, TaskType.ORGANISATION);
+        currentContract.addLink("Stromanbieter wechseln", "https://www.verivox.de"); // FIXME: partner-id
+        Task.addTask(currentContract);
+
+        Task internetContract = new Task("Internetanschluss", "Prüfen Sie, ob Sie Ihren Anbieter wechseln müssen / dürfen. Andernfalls muss dieser zumindest informiert werden.", addMonthDaysToJavaUtilDate(removalDate, 0, -7), Priority.High, 0L, TaskType.ORGANISATION);
+        internetContract.addLink("Internetanbieter wechseln", "https://www.verivox.de"); // FIXME: partner-id
+        internetContract.addLink("alter Anbieter am neuen Wohnort?", "https://www.verivox.de"); // FIXME: link kann ich versorgt werden (Gebiet)
+        Task.addTask(internetContract);
+
+        Task phoneContract = new Task("Festnetztelefon", "Prüfen Sie, ob Sie Ihren Anbieter wechseln müssen / dürfen und ob eine Mitnahme der alten Nummer möglich ist.", addMonthDaysToJavaUtilDate(removalDate, 0, -7), Priority.High, 0L, TaskType.ORGANISATION);
+        phoneContract.addLink("Telefonanbieter wechseln", "https://www.verivox.de"); // FIXME: partner-id
+        Task.addTask(phoneContract);
+
+        Task tvContract = new Task("TV-Anschluss", "Prüfen Sie, ob Sie Ihren Anbieter wechseln müssen / dürfen. Andernfalls muss dieser zumindest informiert werden.", addMonthDaysToJavaUtilDate(removalDate, 0, -7), Priority.High, 0L, TaskType.ORGANISATION);
+        tvContract.addLink("TV-Anbieter wechseln", "https://www.verivox.de"); // FIXME: partner-id
+        Task.addTask(tvContract);
 
         Task fireAndBurglaryProtection = new Task("Brandschutz und Einbruchsschutz", "Rauchmelder sind in einigen Bundesländern Pflicht und können Leben retten. Vergessen Sie zudem nicht den Einbruchsschutz.", removalDate != null ? addMonthDaysToJavaUtilDate(removalDate, 0, -6) : null, Priority.Normal, 0L, TaskType.SECURITY);
         fireAndBurglaryProtection.addLink("Feuermelder", "https://www.amazon.de/Rauchmelder-Brandmelder/b?ie=UTF8&node=2077635031");
